a79780c4db7158c4c004bb0181854e3040b1b977
angeleranz <agpalici@gmail.com>
2023-08-10 20:47:47 -0300

    add functionality to buttons and finish layout

--- javascript.js
+++ javascript.js
@@ -2,6 +2,42 @@ const container = document.querySelector('#container');
 const body = document.body
 const slider = document.querySelector('.size')
 const sliderValueDisplay = document.getElementById('sliderValue');
+const blackButton = document.querySelector('.colorModeBlack');
+const whiteButton = document.querySelector('.colorModeWhite');
+const rainbowButton = document.querySelector('.rainbowMode');
+const eraserButton = document.querySelector('.eraser');
+const resetButton = document.querySelector('.reset');
+
+
+
+blackButton.addEventListener('click', () => {
+    container.addEventListener('mouseover', changeCellColorToBlack);
+    container.removeEventListener('mouseover', changeCellColorToWhite);
+    container.removeEventListener('mouseover', changeCellColorToRainbow);
+})
+
+eraserButton.addEventListener('click', () => {
+    container.addEventListener('mouseover', changeCellColorToWhite);
+    container.removeEventListener('mouseover', changeCellColorToBlack);
+    container.removeEventListener('mouseover', changeCellColorToRainbow);
+})
+
+rainbowButton.addEventListener('click', () => {
+    container.addEventListener('mouseover', changeCellColorToRainbow);
+    container.removeEventListener('mouseover', changeCellColorToBlack);
+    container.removeEventListener('mouseover', changeCellColorToWhite);
+})
+
+resetButton.addEventListener('click', () => {
+    container.innerHTML = '';
+    slider.value = 16;
+    gridSize = 16;
+    sliderValueDisplay.textContent = gridSize;
+    const cellSize = 500 / gridSize;
+    container.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
+    defaultGrid();
+})
 
 slider.addEventListener('input', () => {
     const gridSize = slider.value;
@@ -18,6 +54,32 @@ slider.addEventListener('input', () => {
         createCells();
     }
 })
+
+function changeCellColorToBlack(e) {
+    if (e.target.classList.contains('cell')) {
+        e.target.style.backgroundColor = 'black';
+    }
+}
+
+function changeCellColorToWhite(e) {
+    if (e.target.classList.contains('cell')) {
+        e.target.style.backgroundColor = 'white';
+    }
+}
+
+function changeCellColorToRainbow(e) {
+    if (e.target.classList.contains('cell')) {
+        const randomColor = generateRandomColor();
+        e.target.style.backgroundColor = randomColor;
+    }
+}
+
+function generateRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
 
 function createCells(){
     let cell = document.createElement('div');
